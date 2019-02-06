59335197e90a9d5ce642f2a000fba155d49ebabf
Antonio Ponce <antpon007@gmail.com>
2019-02-06 16:24:56 -0500

    iniciando taller

---
+++ src/components/Task.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="post__container">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">{{description}}</h5>
+        <p class="card-text">{{author}}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "task",
+  props: {
+    description: {
+      type: String,
+      default: ""
+    },
+    author: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
---
+++ src/components/TaskForm.vue
@@ -0,0 +1,54 @@
+<template>
+  <form>
+    <div class="form-group">
+      <label>Description:</label>
+      <textarea class="form-control" rows="3" v-model="description"></textarea>
+    </div>
+    <div class="form-group">
+      <label>Author:</label>
+      <select class="form-control" v-model="userId">
+        <option v-for="user in users" :key="user._id" :value="user._id">{{user.fullname}}</option>
+      </select>
+    </div>
+    <button type="button" class="btn btn-primary" @click="save">Crear</button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "task-form",
+  data() {
+    return {
+      description: "",
+      userId: "",
+      users: []
+    };
+  },
+  created() {
+    fetch($parent.servidor + "users")
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        const { items = [] } = data;
+        const users = items.map(item => {
+          const { _id, firstname = "", lastname = "" } = item;
+          return {
+            _id,
+            fullname: `${firstname} ${lastname}`
+          };
+        });
+
+        this.users = users;
+      });
+  },
+  methods: {
+    save() {
+      this.$emit("save", {
+        description: this.description,
+        userId: this.userId
+      });
+    }
+  }
+};
+</script>
